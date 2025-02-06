06764e1d7f48dde6ad82f2307986b166b3e83eb5
Shivansh Singh <119441847+ShivanshSinghFrosty007@users.noreply.github.com>
2025-02-06 17:52:40 +0530

    session redirect logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,32 @@
 import "./App.css";
-import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const userData = JSON.parse(localStorage.getItem("userdata"))
+
+  const handleLoginClick = () => {
+    if (!userData || !userData.token) {
+      navigate("/login");
+    }else{
+      navigate("/dashboard");
+    }
+  };
+
   return (
     <div>
       {/* Header */}
       <header>
         <div className="header-title">Launchpad</div>
         <div className="auth-buttons">
-          <Link to="/login">
-            <button className="login">Login</button>
-          </Link>
-          <Link to="/signup">
-              <button className="signup">Sign Up</button>
-          </Link>
+          <button className="login" onClick={handleLoginClick}>Login</button>
+          <button className="signup" onClick={() => navigate("/signup")}>Sign Up</button>
         </div>
       </header>
 
       {/* Main Content */}
       <main>
-        {/* <img
-          src="\freepik__the-style-is-candid-image-photography-with-natural__59550.png"
-          alt="Placeholder"
-        /> */}
         <h1>Welcome to Our Platform</h1>
         <p>
           Join us and experience the best service with seamless login and signup options.
